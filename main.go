32a1a16b3b47517e11bb972a81b55eff48c21949
Sergey Kitler <s.kitler@bi.zone>
2024-08-25 22:29:03 +0700

    Add second step

--- main.go
+++ main.go
@@ -1,15 +1,61 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+
+	_ "modernc.org/sqlite"
 )
 
+func createDb(dbName string) {
+	appPath, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbFile := filepath.Join(appPath, dbName)
+	_, err = os.Stat(dbFile)
+
+	var install bool
+	if err != nil {
+		install = true
+	}
+	db, err := sql.Open("sqlite", dbFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.Close()
+	if install {
+		_, err = db.ExecContext(
+			context.Background(),
+			`CREATE TABLE IF NOT EXISTS scheduler (
+					id INTEGER PRIMARY KEY AUTOINCREMENT, 
+					date VARCHAR(8) NOT NULL, 
+					title TEXT NOT NULL, 
+					comment TEXT NULL, 
+					repeat VARCHAR(128) NOT NULL
+					);
+			 CREATE INDEX id ON scheduler (id)`,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
 func main() {
+	createDb("scheduler.db")
+
 	http.Handle("/", http.FileServer(http.Dir("./web")))
 
 	err := http.ListenAndServe(":7540", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
